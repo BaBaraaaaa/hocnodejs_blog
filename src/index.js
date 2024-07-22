d87866b01163498cd7bfc7d6909653ec90683bbc
Lê Toàn Thắng <106722993+BaBaraaaaa@users.noreply.github.com>
2024-07-22 16:14:50 +0700

    uninstall slug genarator -> i slug updater

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const { engine } = require('express-handlebars');
 const path = require('path');
-const {slug} = require('mongoose-slug-generator');
+
 const app = express();
 const port = 3000;
 
